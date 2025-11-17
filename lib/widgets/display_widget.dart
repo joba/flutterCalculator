eb907f045341bfd1b03795693df9bcbe77885029
Jon Bågling <bagling@gmail.com>
2025-11-17 10:54:23 +0100

    add math package, add calculation utils, add label

--- lib/widgets/display_widget.dart
+++ lib/widgets/display_widget.dart
@@ -2,23 +2,36 @@ import 'package:flutter/material.dart';
 
 class DisplayWidget extends StatelessWidget {
   final String text;
+  final String? labelText;
 
-  const DisplayWidget({super.key, required this.text});
+  const DisplayWidget({super.key, required this.text, this.labelText});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.centerRight,
-      margin: const EdgeInsets.all(20),
-      padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(40)),
-        color: Colors.white,
-      ),
-      child: Text(
-        text,
-        style: const TextStyle(color: Colors.black, fontSize: 30),
-      ),
+    return Column(
+      children: [
+        Container(
+          alignment: Alignment.centerRight,
+          margin: const EdgeInsets.fromLTRB(0, 20, 30, 0),
+          child: Text(
+            labelText ?? "",
+            style: TextStyle(color: Colors.grey[600], fontSize: 24),
+          ),
+        ),
+        Container(
+          alignment: Alignment.centerRight,
+          margin: const EdgeInsets.fromLTRB(20, 5, 20, 20),
+          padding: const EdgeInsets.fromLTRB(30, 10, 30, 10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(40)),
+            color: Colors.white,
+          ),
+          child: Text(
+            text,
+            style: const TextStyle(color: Colors.black, fontSize: 30),
+          ),
+        ),
+      ],
     );
   }
 }
